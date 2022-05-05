485eac991aed24a5dab0d442bf930af7f1259c7a
Aman Singh <chandel.s.aman@gmail.com>
2022-05-05 09:23:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+
+submitBtn.addEventListener("click", () => {
+    const val =userName.value;
+    if (val.trim() !== "" && userName.checkValidity()) {
+        // console.log(val);
+        generatePDF(val);
+      } else {
+        userName.reportValidity();
+      }
+});
+const generatePDF = async (name) => {
+    const existingPdfBytes = await fetch("/Certificate.pdf").then((res) =>
+      res.arrayBuffer()
+    );
+
+    // Load a PDFDocument from the existing PDF bytes
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.registerFontkit(fontkit);
+
+    
+  //get font
+  const fontBytes = await fetch("buffalo.otf").then((res) =>
+  res.arrayBuffer()
+);
+  // Embed our custom font in the document
+  const buffalo  = await pdfDoc.embedFont(fontBytes);
+   // Get the first page of the document
+   const pages = pdfDoc.getPages();
+   const firstPage = pages[0];
+ 
+   // Draw a string of text diagonally across the first page
+   const { width, height } = firstPage.getSize()
+   firstPage.drawText(name, {
+    x: width/2 - (width/103) * name.length * 2,
+    y: 290,
+    size: 103,
+    font: buffalo ,
+    color: rgb(0, 0, 0),
+   });
+ 
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  saveAs(pdfDataUri,"certificate.pdf")
+};
